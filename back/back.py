0d4653a02ac3bf82c562d12d5c239cd40cc6eecb
Tudor Boureanu <tudor04boureanu@gmail.com>
2025-10-25 19:25:45 +0300

    Made querying from the database work

--- back/back.py
+++ back/back.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
-from sqlalchemy import create_engine, Integer, String, Boolean, CHAR, Date, Sequence, Text, Float, Uuid
-from sqlalchemy.orm import sessionmaker, DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import create_engine, select, Integer, String, Boolean, CHAR, Date, Sequence, Text, Float, Uuid
+from sqlalchemy.orm import Session, DeclarativeBase, Mapped, mapped_column
 from datetime import date
+import json
+
+# Makes objected resulted from sqlalchemy statement execution into a dictionary
+def model_to_dict(obj):
+    return {c.key: getattr(obj, c.key) for c in obj.__mapper__.columns}
 
 class Base(DeclarativeBase):
     pass
@@ -40,7 +45,11 @@ class POS_LOG(Base):
 app = FastAPI()
 database_url = "postgresql://dvloperhackdb:dvloperdbpass@0.0.0.0/dvloperhackdb"
 engine = create_engine(database_url)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-
-
+@app.get("/transactions")
+async def get_transactions():
+    with Session(engine) as session:
+        objs = session.scalars(select(POS_LOG)).all()
+        data = [model_to_dict(o) for o in objs]
+    return data
+        
